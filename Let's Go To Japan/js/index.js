a985f2111533ab109de26503ac9634082bc5619d
lilymadojemu <70119291+lilymadojemu@users.noreply.github.com>
2023-04-07 20:27:22 -0400

    Beginning to finalize the map

--- Let's Go To Japan/js/index.js
+++ Let's Go To Japan/js/index.js
@@ -5,7 +5,7 @@ var map = L.map('map').setView([35.6246, 139.4294], 13);
 
 // Adding a tile layer to the map using OpenStreetMap tile layer 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 60,
+    maxZoom: 100,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
@@ -13,7 +13,7 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 // Adding multiple markers
 var sanrioMarker = L.marker([35.6246, 139.4294], { className:"customizedMarker"}).addTo(map);
 
-var inariMarker = L.marker([34.9671, 135.772], { className:"customizedMarker"}).addTo(map);
+var inariMarker = L.marker([34.9671, 135.772], {className:"customizedMarker"}).addTo(map);
 
 var castleMarker = L.marker([34.6873, 135.5259],{className:"customizedMarker"}).addTo(map);
 
@@ -22,11 +22,11 @@ var castleMarker = L.marker([34.6873, 135.5259],{className:"customizedMarker"}).
 // openPopup: immediately opens the attached popup for markers only.
 
 // Sanrio Marker Popup
-sanrioMarker.bindPopup('<div><img src="location-images/Sanrio/sanrio_characters_image.jpeg" class="markerImg" alt="Sanrio Characters">  <a href="destination.html"> <p class="markerText">Welcome to Sanrio PuroLand!</p></a></div>').openPopup();
+sanrioMarker.bindPopup('<div><img src="location-images/Sanrio/sanrio_characters_image.jpeg" class="markerImg" alt="Sanrio Characters"></div> <div><a href="destinationInDepth.html"> <p class="markerText"> Sanrio Puroland </p></a></div>').openPopup();
 // Fushimi Inari Marker Popup 
-inariMarker.bindPopup('<div><img src="location-images/Inari/stefan-k-62IRMCiDaPY-unsplash.jpg" class="markerImg" alt="Fushimi Inari Shrine"> <p class="markerText"> Welcome to Fushimi Inari Shrine! </p></div>').openPopup();
+inariMarker.bindPopup('<div><img src="location-images/Inari/stefan-k-62IRMCiDaPY-unsplash.jpg" class="markerImg" alt="Fushimi Inari Shrine"></div> <div><a href="destinationInDepth.html"> <p class="markerText"> Fushimi Inari Shrine</p></a></div>').openPopup();
 // Osaka Castle Marker Popup
-castleMarker.bindPopup('<div><img src="location-images/Castle/cosmin-serban-kUQPX2kp8S8-unsplash.jpg" class="markerImg" alt="Osake Castle"> <p class="markerText"> Welcome to Osaka Castle</p></div>').openPopup();
+castleMarker.bindPopup('<div><img src="location-images/Castle/cosmin-serban-kUQPX2kp8S8-unsplash.jpg" class="markerImg" alt="Osaka Castle"> </div> <div><a href="destinationInDepth.html"> <p class="markerText"> Osaka Castle </p></a></div>').openPopup();
 
 
 // bindTooltip: attaches a tooltip with the specified HTML content to your marker so the tooltip appears when you hover over the marker
@@ -41,7 +41,8 @@ castleMarker.bindTooltip('Osaka Castle').openTooltip();
 
 // Close all popups on page load
 map.eachLayer(function (layer) {
-    if (layer instanceof L.Popup) {
+    if (layer instanceof L.Popup) or (layer instanceof L.bindPopup); {
         layer.closePopup();
     }
 });
+
