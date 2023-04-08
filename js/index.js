fe1bb5882cebc931ee4b99a718b9ac61b5e6d938
lilymadojemu <70119291+lilymadojemu@users.noreply.github.com>
2023-04-07 21:06:15 -0400

    Trying the slideshow

--- js/index.js
+++ js/index.js
@@ -1,7 +1,45 @@
-// Japan Map
+// // Japan Map
 
+// // SlideShow Photo Data
+// var sanrioPhotos = [
+//     {
+//       src: 'location-images/Sanrio/sanrio_characters_image.jpeg',
+//       title: 'Sanrio Characters'
+//     },
+//     {
+//       src: 'location-images/Sanrio/sanrio_puroland.jpg',
+//       title: 'Sanrio PuroLand'
+//     }
+// ];
+  
+// var inariPhotos = [
+// {
+//     src: 'location-images/Inari/stefan-k-62IRMCiDaPY-unsplash.jpg',
+//     title: 'Fushimi Inari Shrine'
+// },
+// {
+//     src: 'location-images/Inari/fushimi_inari_shrine_night.jpg',
+//     title: 'Fushimi Inari Shrine at night'
+// }
+// ];
+
+// var castlePhotos = [
+// {
+//     src: 'location-images/Castle/cosmin-serban-kUQPX2kp8S8-unsplash.jpg',
+//     title: 'Osaka Castle'
+// },
+// {
+//     src: 'location-images/Castle/osaka_castle_cherry_blossom.jpg',
+//     title: 'Osaka Castle during cherry blossom season'
+// }
+// ];
+
+// sanrioMarker.photo = sanrioPhotos;
+// inariMarker.photo = inariPhotos;
+// castleMarker.photo = castlePhotos;
+  
 // Intializing the Map using specific coordinates to set view to Tokyo Japan
-var map = L.map('map').setView([35.6246, 139.4294], 13);
+var map = L.map('map').setView([35.6246, 139.4294], 7);
 
 // Adding a tile layer to the map using OpenStreetMap tile layer 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -11,11 +49,11 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 
 // Adding marker to map
 // Adding multiple markers
-var sanrioMarker = L.marker([35.6246, 139.4294], { className:"customizedMarker"}).addTo(map);
+var sanrioMarker = L.marker([35.6246, 139.4294]).addTo(map);
 
-var inariMarker = L.marker([34.9671, 135.772], {className:"customizedMarker"}).addTo(map);
+var inariMarker = L.marker([34.9671, 135.772]).addTo(map);
 
-var castleMarker = L.marker([34.6873, 135.5259],{className:"customizedMarker"}).addTo(map);
+var castleMarker = L.marker([34.6873, 135.5259]).addTo(map);
 
 // bindPopup: attaches a popup with the specified HTML content to your marker so the popup appears when you click on the object
 // with bindPopUp link to "preview" of destination: img, the destiantion name, and location
@@ -24,7 +62,7 @@ var castleMarker = L.marker([34.6873, 135.5259],{className:"customizedMarker"}).
 // Sanrio Marker Popup
 sanrioMarker.bindPopup('<div><img src="location-images/Sanrio/sanrio_characters_image.jpeg" class="markerImg" alt="Sanrio Characters"></div> <div><a href="destinationInDepth.html"> <p class="markerText"> Sanrio Puroland </p></a></div>').openPopup();
 // Fushimi Inari Marker Popup 
-inariMarker.bindPopup('<div><img src="location-images/Inari/stefan-k-62IRMCiDaPY-unsplash.jpg" class="markerImg" alt="Fushimi Inari Shrine"></div> <div><a href="destinationInDepth.html"> <p class="markerText"> Fushimi Inari Shrine</p></a></div>').openPopup();
+inariMarker.bindPopup('<div><img src="location-images/Inari/stefan-k-62IRMCiDaPY-unsplash.jpg" class="markerImg" alt="Fushimi Inari Shrine"> </div> <div><a href="destinationInDepth.html"> <p class="markerText"> Fushimi Inari Shrine</p></a></div>').openPopup();
 // Osaka Castle Marker Popup
 castleMarker.bindPopup('<div><img src="location-images/Castle/cosmin-serban-kUQPX2kp8S8-unsplash.jpg" class="markerImg" alt="Osaka Castle"> </div> <div><a href="destinationInDepth.html"> <p class="markerText"> Osaka Castle </p></a></div>').openPopup();
 
@@ -33,7 +71,7 @@ castleMarker.bindPopup('<div><img src="location-images/Castle/cosmin-serban-kUQP
 // with bindTooltip link to "preview" of destination: img, the destination name, and location
 
 // Sanrio Marker Tooltip
-sanrioMarker.bindTooltip('Sanrio PuroLand').openTooltip();
+sanrioMarker.bindTooltip('Sanrio Puroland').openTooltip();
 // Fushimi Inari Marker Tooltip
 inariMarker.bindTooltip('Fushimi Inari Temple').openTooltip();
 // Osaka Castle Marker Tooltip
