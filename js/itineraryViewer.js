409b5c1683bdd96feb7c78052137d6e23e55172a
lilymadojemu <70119291+lilymadojemu@users.noreply.github.com>
2023-04-23 17:33:00 -0400

    Getting Calendar.js

--- js/itineraryViewer.js
+++ js/itineraryViewer.js
@@ -1,8 +1,4 @@
-
-
-var timetable = new Timetable(document.getElementById('timetable'));
-
-timetable.addEvent('English Class', 'Room 101', new Date(2023, 3, 24, 9, 0), new Date(2023, 3, 24, 11, 0), 1, null);
-
-timetable.draw();
-
+var calendarInstance1 = calendarJs( "calendar", {
+    manualEditingEnabled: true
+    // All your options can be set here
+} );
